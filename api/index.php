60017a3f652620d6a745da1fe85600e92df808a4
PatPk-dev <ppp.patpk@gmail.com>
2026-05-22 09:12:46 +0700

    Optimize Laravel path caching for serverless read-only environment on Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
 // Forward Vercel serverless requests to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
